d2358f98848590cb544b82f5613395cb7832934e
Marco Brandizi <marco@leafbioscience.com>
2011-06-24 15:29:58 +0100

    Merging from upstream, integrating recent changes to graph2tab, jutils.

--- model/run_tests.sh
+++ model/run_tests.sh
@@ -1,5 +1,9 @@
-mvn -DargLine="-Xms512m -Xmx4G -XX:PermSize=128m -XX:MaxPermSize=256m" \
+# I need to live with two Maven versions, usually the first will be just fine
+MVN=mvn 
+#MVN="mvn2 -s $HOME/.m2_old/settings.xml"
+
+$MVN -DargLine="-Xms512m -Xmx4G -XX:PermSize=128m -XX:MaxPermSize=256m" \
     -Dsurefire.useFile=true \
     -Ph2,test clean test
 
-mvn surefire-report:report-only
+$MVN surefire-report:report-only
