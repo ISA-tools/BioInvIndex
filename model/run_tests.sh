bf5811b89c6259f9112727d0ff6c1c4a23a8d459
Marco Brandizi <marco@leafbioscience.com>
2011-10-03 21:15:45 +0100

    Improvement on run_tests.sh.

--- model/run_tests.sh
+++ model/run_tests.sh
@@ -1,5 +1,8 @@
-# I need to live with two Maven versions, usually the first will be just fine
-MVN=mvn 
+# I need to live with two Maven versions, usually leaving MVN unset will be just fine
+if [ "$MVN" == "" ]; then
+  echo "Setting 'mvn' as default Maven command, you possibly need to setup Maven 2 via export MVN=<path> before invoking me"
+  MVN=mvn
+fi 
 #MVN="mvn2 -s $HOME/.m2_old/settings.xml"
 
 $MVN -DargLine="-Xms512m -Xmx4G -XX:PermSize=128m -XX:MaxPermSize=256m" \
