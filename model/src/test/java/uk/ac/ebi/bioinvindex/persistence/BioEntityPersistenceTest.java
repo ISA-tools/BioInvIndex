131bc327aa8a0089f9c33233038c84571f516f2c
Eamonn Maguire <eamonnmag@gmail.com>
2012-02-04 09:56:06 +0000

    Further changes to get closer to having a functioning test for this. Struggling with adding/saving/persisting nodes at the right time.

--- model/src/test/java/uk/ac/ebi/bioinvindex/persistence/BioEntityPersistenceTest.java
+++ model/src/test/java/uk/ac/ebi/bioinvindex/persistence/BioEntityPersistenceTest.java
@@ -8,7 +8,11 @@ import uk.ac.ebi.bioinvindex.model.AssayResult;
 import uk.ac.ebi.bioinvindex.model.BioEntity;
 import uk.ac.ebi.bioinvindex.model.Data;
 import uk.ac.ebi.bioinvindex.model.Study;
+import uk.ac.ebi.bioinvindex.model.processing.DataNode;
 import uk.ac.ebi.bioinvindex.model.term.DataType;
+import uk.ac.ebi.bioinvindex.model.xref.ReferenceSource;
+import uk.ac.ebi.bioinvindex.persistence.pipeline.DataNodePersister;
+import uk.ac.ebi.bioinvindex.persistence.pipeline.DataPersister;
 import uk.ac.ebi.bioinvindex.utils.test.TransactionalDBUnitEJB3DAOTest;
 
 import java.sql.Timestamp;
@@ -50,33 +54,69 @@ public class BioEntityPersistenceTest extends TransactionalDBUnitEJB3DAOTest {
 
         out.println("\n\n\n _______________  testPersistMetabolites, ____________________________\n");
 
+        // persist study
         Study study = new Study("Testing MetaboLights Entities");
+        study.setAcc("test-bioentity");
+        Study studyDB = persister.persist(study);
 
-        DataType dataType = new DataType("acc", "name", null);
+        System.out.println("STUDY PERSISTED");
+
+        // persist our reference source
+        ReferenceSource referenceSource = new ReferenceSource("blah:acc", "myData");
+        ReferenceSourcePersister referenceSourcePersister = new ReferenceSourcePersister(DaoFactory.getInstance(entityManager), new Timestamp(System.currentTimeMillis()));
+        referenceSourcePersister.persist(referenceSource);
+
+        System.out.println("REFERENCE SOURCE PERSISTED");
+
+        // persist our data
+        DataType dataType = new DataType("123321", "name", referenceSource);
+        OntologyEntryPersister<DataType> ontologyEntryPersister = new OntologyEntryPersister<DataType>(DaoFactory.getInstance(entityManager), new Timestamp(System.currentTimeMillis())) {};
+        ontologyEntryPersister.save(dataType);
+        System.out.println("DATA TYPE SAVED");
+
         Data data = new Data("http://www.ebi.ac.uk/bioinvindex", dataType);
+        data.setAcc("myAcc");
+        DataPersister dataPersister = new DataPersister(DaoFactory.getInstance(entityManager), new Timestamp(System.currentTimeMillis()));
+        dataPersister.save(data);
 
-        AssayResult result = new AssayResult(data, study, new ArrayList<BioEntity>());
+        System.out.println("DATA SAVED");
+
+        // persist our processing node
+        DataNode node = new DataNode(study, true);
+        node.setAcc("mynode-acc");
+        node.setData(data);
+        DataNodePersister dataNodePersister = new DataNodePersister(DaoFactory.getInstance(entityManager), new Timestamp(System.currentTimeMillis()));
+        dataNodePersister.persist(node);
+
+        System.out.println("DATA NODE PERSISTED");
+
+        data.setProcessingNode(node);
+
+        dataPersister.persist(data);
+
+        System.out.println("DATA PERSISTED");
+
+        // persist our data
+        AssayResult result = new AssayResult(null, study, new ArrayList<BioEntity>());
+        AssayResultPersister assayResultPersister = new AssayResultPersister(DaoFactory.getInstance(entityManager), new Timestamp(System.currentTimeMillis()));
 
         BioEntity bioEntity1 = new BioEntity("water", "CHEBI:15377");
-
         result.addBioEntity(bioEntity1);
 
         study.addAssayResult(result);
 
+        assayResultPersister.persist(result);
+        System.out.println("ASSAY RESULT PERSISTED");
         // Let's go with the persistence job!
 
-        Study studyDB = persister.persist(study);
         transaction.commit();
-
-        assertNotNull("Oh! No study returned by the persister!", studyDB);
-
 
         assertNotNull("Oh! No study returned by the persister!", studyDB);
         assertEquals("Ouch! The study rerturned by the persister should be the original one!", study, studyDB);
         assertTrue("Argh! Cannot find the persisted study in the DB!", entityManager.contains(studyDB));
         assertNotNull("Urp! The study should have an ID", studyDB.getId());
 
-        // Test the assaygroup
+        // Test the bioentity
         Collection<AssayResult> assayResults = studyDB.getAssayResults();
         assertNotNull("Ops! The retuned study has no an assayGroup!", assayResults);
         assertEquals("Oh no! Bad no. of assayGroups persisted!", 1, assayResults.size());
